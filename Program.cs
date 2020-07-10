c9e10d8312a70c7ddc059842ffe6cd830ddb14cb
wojwoz <61656116+wojwoz@users.noreply.github.com>
2020-07-10 23:04:30 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wisielec_wsei_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
