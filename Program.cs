fd16cf1eded9c4ed685cfa57948fff453d37a413
wojwoz <61656116+wojwoz@users.noreply.github.com>
2020-07-12 13:17:01 +0200

    add words table

--- Program.cs
+++ Program.cs
@@ -44,12 +44,55 @@ namespace wisielec_wsei_2020
             string answer = Console.ReadLine();
             if (answer == "t")
             {
-                Console.WriteLine("lol");
+                Console.WriteLine("Zaczynamy grę!");
             }
             else
             {
                 Console.WriteLine("\tPierwszy gracz wymyśla słowo, ujawniając na przykład za pomocą poziomych kresek liczbę tworzących je liter. \nDrugi z graczy stara się odgadnąć litery słowa. Za każdym razem, gdy mu się to uda, pierwszy gracz wstawia literę w odpowiednie miejsce; \nw przeciwnym wypadku rysuje element symbolicznej szubienicy i wiszącego na niej ludzika. Jeżeli pierwszy gracz narysuje kompletnego „wisielca” zanim drugi odgadnie słowo, wówczas wygrywa. \nW zależności od wcześniej ustalonego stopnia skomplikowania rysunku „wisielca” (liczba elementów potrzebna do jego narysowania), gra pozwala na mniej lub więcej pomyłek odgadującego[1].");
             }
+
+            Console.WriteLine("Losuj słowo");
+            Console.ReadKey();
+            Random los = new Random();
+
+            string[] words = {
+                "Alaska",
+                "Altana",
+                "Arbuz",
+                "Arystokracja",
+                "Ameba",
+                "banan",
+                "baran",
+                "bałwan",
+                "bermudy",
+                "boczek",
+                "czrownica",
+                "czas",
+                "czaszka",
+                "cytryna",
+                "czapka",
+                "dom",
+                "drzewo",
+                "drut",
+                "drabina",
+                "dywan",
+                "ewakuacja",
+                "epopeja",
+                "europa",
+                "Etiopia",
+                "Egipt",
+                "flet",
+                "finka",
+                "farba",
+                "flaga",
+                "falbana"
+            };
+
+
+            int wordID = los.Next(words.Length);
+            Console.WriteLine(words[wordID]);
+           
+          
 
 
 
